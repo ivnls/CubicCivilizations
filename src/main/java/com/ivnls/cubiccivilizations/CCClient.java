65cc089d0a43a5cd780af7e407a30dcd17b222db
ivnls <il4820728@gmail.com>
2024-12-21 13:21:57 -0300

    Initial commit

---
+++ src/main/java/com/ivnls/cubiccivilizations/CCClient.java
@@ -0,0 +1,11 @@
+package com.ivnls.cubiccivilizations;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CCClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/ivnls/cubiccivilizations/CubicCivilizations.java
@@ -0,0 +1,16 @@
+package com.ivnls.cubiccivilizations;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CubicCivilizations implements ModInitializer {
+	public static final String MOD_ID = "cubic-civilizations";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
